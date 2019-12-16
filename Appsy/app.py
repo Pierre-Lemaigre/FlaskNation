4728e970306b75e4f6d0184142e7aa576067f7fd
ArnaudSaulou <a.saulou@gmail.com>
2019-12-16 01:17:18 +0100

    Connection + 404 + Home_patient

--- Appsy/app.py
+++ Appsy/app.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+from datetime import  datetime
 
 app = Flask(__name__)
+Bootstrap(app)
 
+@app.context_processor
+def inject_now():
+    return dict(now=datetime.now())
+
+@app.errorhandler(404)
+def pageNotFound(erreur):
+    return render_template('errors/404.html'), 404
 
 @app.route('/')
-def hello_world():
-    return 'Hello World !'
+def goToConnection():
+    return render_template('pages/connection.html')
 
+@app.route('/home', methods=['POST'])
+def goToHome():
+    user = request.form['user']
+    if(user == "admin"):
+        return render_template('pages/home_psy.html')
+    else:
+        return render_template('pages/home_patient.html', user=user)
 
 if __name__ == '__main__':
     app.run()
